aabb859e120d41a2e179ca5e0e4209950985d827
PeatChatchawan <peat.chatchawan@gmail.com>
2023-04-21 17:06:06 +0700

    fix

--- src/app/pages/person/person.page.spec.ts
+++ src/app/pages/person/person.page.spec.ts
@@ -5,11 +5,11 @@ describe('PersonPage', () => {
   let component: PersonPage;
   let fixture: ComponentFixture<PersonPage>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     fixture = TestBed.createComponent(PersonPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,21 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+// Import the Firebase SDK
+import 'firebase/compat/firestore';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  // Add your Firebase project configuration here
+  projectId: 'absdata-46619',
+  appId: '1:252976268944:web:499a9a76198c76ddce7a15',
+  storageBucket: 'absdata-46619.appspot.com',
+  apiKey: 'AIzaSyDvnnMp0ixuQbTg27nFqa8z_BgZQQXVLXk',
+  authDomain: 'absdata-46619.firebaseapp.com',
+  messagingSenderId: '252976268944',
+};
 
 if (environment.production) {
   enableProdMode();
@@ -16,11 +29,13 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    importProvidersFrom(
-      IonicModule.forRoot({}),
-      AngularFireModule.initializeApp(environment.firebase),
-      AngularFirestoreModule
-    ),
+    importProvidersFrom(IonicModule.forRoot({})),
     provideRouter(routes),
   ],
 });
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
